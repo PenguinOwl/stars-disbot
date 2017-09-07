732251c8b8f454cb957b429a97a82d2446da9d60
Henry <henryjboles@gmail.com>
2017-09-07 14:15:10 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,7 @@
 require "discordrb"
 #tba
+bot = Discordrb::Bot.new token: 'RnMZ3_G2ydLoYM7QFLf9aH0uyI2bvF_-NBVwRj6Vr1Gb4gJ4vT27AGM5FEenSuhKmV6r', client_id: 355460755627442177
+bot.message(content: 'Ping!') do |event|
+  event.respond 'Pong!'
+end
+bot.run
