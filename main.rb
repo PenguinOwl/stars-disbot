e20cbe58f5e57e098a9eca3d1f1eef6b5f2314b8
Henry <henryjboles@gmail.com>
2018-01-26 16:10:21 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,22 @@
 require "discordrb"
 #tba
-puts "hi"
+prefix = "["
 bot = Discordrb::Bot.new token: 'MzU1NTA4NzI4MzQ0MDg0NTEx.DJN1kg.hqg0FnhVzEKfLE2TVl8y6naurJs', client_id: 355508728344084511
-bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
+
+def command(command,event,args)
+  catch ArgumentError
+    send(command,event,*args)
+  rescue
+    bot.sendmessage("woow")
+  end
+end
+
+bot.message(startwith: prefix) do |event|
+  cmd = event.message.content
+  cmd[0] = ""
+  cmd = cmd.split(" ")
+  top = cmd[0]
+  cmd.delete_at(0)
+  command(top, event, cmd)
 end
 bot.run
