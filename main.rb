f9cd3f7708ea330ba1d980ee2d07857b9a78c32e
BuildTools <henryjboles@gmail.com>
2018-01-28 06:19:44 -0800

    finally

--- main.rb
+++ main.rb
@@ -48,4 +48,20 @@ Thread.new {while gets=="stop" do bot.stop end}
   #       END OF COMMANDS
   #-----------------------------
 
-  bot.run
+  bot.run :async
+
+# http_server.rb
+require 'socket'
+server = TCPServer.new 5678
+
+while session = server.accept
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print "\r\n" # 3
+  session.print "Hello world! The time is #{Time.now}" #4
+
+  session.close
+end
