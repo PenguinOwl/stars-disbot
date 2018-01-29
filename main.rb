d95b8a53908d52a4f590424e0e9ed6912e9aef84
Henry <henryjboles@gmail.com>
2018-01-28 18:56:50 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -49,21 +49,4 @@ Thread.new {while gets=="stop" do bot.stop end}
   #       END OF COMMANDS
   #-----------------------------
 
-  bot.run :async
-
-# http_server.rb
-require 'socket'
-server = TCPServer.new(ARGV[0].to_i)
-puts p server
-
-while session = server.accept
-  request = session.gets
-  puts request
-
-  session.print "HTTP/1.1 200\r\n" # 1
-  session.print "Content-Type: text/html\r\n" # 2
-  session.print "\r\n" # 3
-  session.print "Hello world! The time is #{Time.now}" #4
-
-  session.close
-end
+  bot.run 
