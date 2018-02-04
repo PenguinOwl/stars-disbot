3287b3554d41074cba196888f1a48c88750aaf84
Henry <henryjboles@gmail.com>
2018-02-03 22:22:27 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -34,7 +34,7 @@ $bot.message do |event|
   if nick
     event.respond(nick)
     require 'net/http'
-    source = Net::HTTP.get('plancke.io', '/hypixel/player/stats/#{nick.match(/\w+/)}')
+    source = Net::HTTP.get('plancke.io', '/hypixel/player/stats/#{nick.match(/\W+/)[0]}')
     puts source.size
     puts "plancke.io" + "/hypixel/player/stats/#{nick.match(/\w+/)}"
     lvl = source.match(/Current Level:<\/b> (\d+)/)
