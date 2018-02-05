6efd1030600bad6625f14b6050b4b4be82595bd9
Henry <henryjboles@gmail.com>
2018-02-04 17:52:28 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -32,8 +32,8 @@ $bot.message do |event|
   nick = event.author.nick
   if nick
     require 'net/http'
-    source = Net::HTTP.get URI("https://plancke.io/hypixel/player/stats/#{nick.match(/(\w+)/i)[1]}")
-    puts "https://plancke.io/hypixel/player/stats/#{nick.match(/(\w+)/i)[1]}"
+    source = Net::HTTP.get URI("https://plancke.io/hypixel/player/stats/#{nick.match(/(\w+)/i)[2]}")
+    puts "https://plancke.io/hypixel/player/stats/#{nick.match(/(\w+)/i)[2]}"
     lvl = source.match(/Current Level:<\/b> (\d+)/)
     nlvl = lvl[1].to_i
     pres = case nlvl
