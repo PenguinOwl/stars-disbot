8f404e17eab4e67cad19b98a603c682365a0cbee
Henry <henryjboles@gmail.com>
2018-03-30 07:29:39 -1000

    Update main.rb

--- main.rb
+++ main.rb
@@ -110,11 +110,8 @@ class Command
   
   def Command.total(event)
     total = 0
-    event.respond ":::"
     event.channel.server.members.each do |mem|
-      event.respond "::"
       if mem.nick
-        event.respond ":a:"
         str = mem.nick.match(/\[(\d+)\s?.?\]\s.+/i)
         if str[1]
           total = str[1].to_i + total
