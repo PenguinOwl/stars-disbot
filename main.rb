5e7c23af63eb93d1155970d0aa2d850c846f0964
Henry <henryjboles@gmail.com>
2018-03-31 09:12:56 -1000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'discordrb'
+require 'hypixel-ruby'
 #tba
 prefix = '='
+$api = API.new(ENV['KEY'])
 puts "key", ENV['KEY']
 $bot = Discordrb::Bot.new token: ENV['KEY'].strip, client_id: ENV['CLIENT'].strip
 puts $bot.invite_url
